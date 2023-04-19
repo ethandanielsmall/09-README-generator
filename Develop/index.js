faab7913c4767d56150cbb51467732e84562aceb
Ethan Daniel <e.daniel.small@gmail.com>
2023-04-18 22:00:57 -0700

    adding prompts for info

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,71 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+    {
+        type: 'input',
+        message: 'xxx',
+        name: 'xxx'
+    },
+];
+
+// .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     },
+//     {
+//       type: 'password',
+//       message: 'What is your password?',
+//       name: 'password',
+//     },
+//     {
+//       type: 'password',
+//       message: 'Re-enter password to confirm:',
+//       name: 'confirm',
+//     },
+//   ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
