2f4abb7e88c0c360e78da637867c529edf404975
Ethan Daniel <e.daniel.small@gmail.com>
2023-04-21 14:00:42 -0700

    functional readme generation and formatting!!!

--- Develop/index.js
+++ Develop/index.js
@@ -49,10 +49,52 @@ text = "this is the text"
 
 // TODO: Create a function to write README file
 
+function writeReadme(answers) {
+    return `
+  # ${answers.title}
+  
+  ## Description
+  
+  ${answers.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${answers.install}
+  
+  ## Usage
+  
+  ${answers.usage}
+  
+  ## Contributing
+  
+  ${answers.contribution}
+  
+  ## Tests
+  
+  ${answers.testing}
+  
+  ## Questions
+  
+  If you have any questions, please feel free to contact me via GitHub or LinkedIn:
+  
+  - GitHub: [${answers.github}](https://github.com/${answers.github})
+  - LinkedIn: [${answers.linkedin}](https://www.linkedin.com/in/${answers.linkedin}/)
+    `;
+  }
+
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
-      fs.writeFile(`${answers.title}.md`, answers.description, (err) => {
+        const readmeContent = writeReadme(answers);
+        fs.writeFile(`${answers.title}.md`, readmeContent, (err) => {
         if (err) throw err;
         console.log('File written successfully!');
       });
